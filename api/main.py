1d1d1c7e6e80b7982e7214a08e73d7dd59a080e5
Kishore <kishore22705@gmail.com>
2025-06-19 00:41:21 +0530

    test

--- api/main.py
+++ api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return JSONResponse(content={"message": "Hello from VidRot API!"})
+    return JSONResponse(content={"message": "Hello from FastAPI on Vercel!"})
+
+handler = Mangum(app)
